d9977402b3b674a66d117c91051ab69d319d5648
BrunoFelipe17 <bruno.felipe7170@gmail.com>
2021-07-24 13:13:45 -0300

    Created user struct

--- lib/users/user.ex
+++ lib/users/user.ex
@@ -3,7 +3,10 @@ defmodule Flightex.Users.User do
   @enforce_keys @keys
   defstruct @keys
 
-  def build do
+  def build(name, email, cpf) when is_bitstring(cpf) do
     # TO DO
+    {:ok, %__MODULE__{id: UUID.uuid4(), name: name, email: email, cpf: cpf}}
   end
+
+  def build(_name, _email, cpf) when is_number(cpf), do: {:error, "Cpf must be a String"}
 end
